3123c34f1436aaf5b24e3c77161e33d18f37dee3
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-21 08:04:34 +0800

    Update SettingsView.swift

--- Dopamine/Dopamine/UI/Views/SettingsView.swift
+++ Dopamine/Dopamine/UI/Views/SettingsView.swift
@@ -25,7 +25,6 @@ struct SettingsView: View {
     @State var mobilePasswordInput = "alpine"
 
     @AppStorage("noUpdates", store: dopamineDefaults()) var noUpdates: Bool = false
-    @AppStorage("bridgeToXinA", store: dopamineDefaults()) var bridgeToXinA: Bool = false
     @State var removeJailbreakAlertShown = false
     @State var isSelectingPackageManagers = false
     @State var tweakInjectionToggledAlertShown = false
@@ -55,7 +54,6 @@ struct SettingsView: View {
                                     }
                                 }
                             if !isJailbroken() {
-                                Toggle("Options_bridgeToXinA", isOn: $bridgeToXinA)
                                 Toggle("Options_Rebuild_Environment", isOn: $rebuildEnvironment)
                                 Toggle("Settings_iDownload", isOn: $enableiDownload)
                                 Toggle("Settings_Verbose_Logs", isOn: $verboseLogs)
