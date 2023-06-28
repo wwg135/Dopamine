84273fa33715c3cfa6700a8f36a12f148b3b2336
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-28 08:19:15 +0800

    Delete mount

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -141,10 +141,6 @@ func jailbreak(completion: @escaping (Error?) -> ()) {
     }
 }
 
-func removeZmount(rmpath: String) {
-    _ = execCmd(args: [CommandLine.arguments[0], "uninstall_Zmount", rmpath])
-}
-
 func removeZplist() {
     _ = execCmd(args: [CommandLine.arguments[0], "uninstall_Zplist"])
 }
