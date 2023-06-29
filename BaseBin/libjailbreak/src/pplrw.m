eddf5b97b85e1d24eef9a3833ccad80374c15f08
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-06-29 15:38:51 +0800

    Update pplrw.m

--- BaseBin/libjailbreak/src/pplrw.m
+++ BaseBin/libjailbreak/src/pplrw.m
@@ -143,7 +143,8 @@ void windowWaitUntilMapped(PPLWindow *window, uint64_t page)
 			worked = true;
 			break;
 		}
-
+        }
+	
 	if (!worked) {
 		// If shit is broken, just do an old flavour flush (not aware of a single instance of this ever not working) and continue
 		tlbFlush();
