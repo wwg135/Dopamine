8903a6d6cfeaf27f22994c5cc19ff56c16e602cf
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-07-14 23:08:21 +0800

    1.1.1 bridge

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -62,6 +62,29 @@ func reboot() {
     _ = execCmd(args: [CommandLine.arguments[0], "reboot"])
 }
 
+func doReboot() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+
+    // MARK: Fade out Animation
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        guard let rebootPath = rootifyPath(path: "/usr/sbin/reboot") else {
+            return
+        }
+        _ = execCmd(args: [rebootPath])
+    })
+}
+
 func isJailbroken() -> Bool {
     if isSandboxed() { return false } // ui debugging
     
@@ -114,6 +137,14 @@ func jailbreak(completion: @escaping (Error?) -> ()) {
     }
 }
 
+func removeZmount(rmpath: String) {
+    _ = execCmd(args: [CommandLine.arguments[0], "uninstall_Zmount", rmpath])
+}
+
+func removeZplist() {
+    _ = execCmd(args: [CommandLine.arguments[0], "uninstall_Zplist"])
+}
+
 func removeJailbreak() {
     dopamineDefaults().removeObject(forKey: "selectedPackageManagers")
     _ = execCmd(args: [CommandLine.arguments[0], "uninstall_environment"])
@@ -141,6 +172,16 @@ func changeMobilePassword(newPassword: String) {
     _ = execCmd(args: [dashPath, "-c", String(format: "printf \"%%s\\n\" \"\(newPassword)\" | \(pwPath) usermod 501 -h 0")])
 }
 
+func newMountPath(newPath: String) {// zqbb_flag
+    let plist = NSDictionary(contentsOfFile: "/var/mobile/newFakePath.plist")
+    let pathArray = plist?["path"] as? [String]
+    if pathArray?.firstIndex(of: newPath) == nil {
+	guard let jbctlPath = rootifyPath(path: "/basebin/jbctl") else {
+            return
+        }
+        _ = execCmd(args: [jbctlPath, "mountPath", newPath])
+    }
+}
 
 func changeEnvironmentVisibility(hidden: Bool) {
     if hidden {
@@ -179,6 +220,26 @@ func updateEnvironment() {
     jbdUpdateFromBasebinTar(Bundle.main.bundlePath + "/basebin.tar", true)
 }
 
+func doUpdateEnvironment() {
+    UIImpactFeedbackGenerator(style: .soft).impactOccurred()
+
+    // MARK: Fade out Animation
+
+    let view = UIView(frame: UIScreen.main.bounds)
+    view.backgroundColor = .black
+    view.alpha = 0
+
+    for window in UIApplication.shared.connectedScenes.map({ $0 as? UIWindowScene }).compactMap({ $0 }).flatMap({ $0.windows.map { $0 } }) {
+        window.addSubview(view)
+        UIView.animate(withDuration: 0.2, delay: 0, animations: {
+            view.alpha = 1
+        })
+    }
+
+    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2, execute: {
+        jbdUpdateFromBasebinTar(Bundle.main.bundlePath + "/basebin.tar", true)
+    })
+}
 
 // debugging
 func isSandboxed() -> Bool {
