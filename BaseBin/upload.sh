6f81983701a11e5faafaf5686492f6516ff51db9
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-07-18 16:59:48 +0800

    Create upload.sh

---
+++ BaseBin/upload.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+./pack.sh
+
+DEVICE=root@iphone13.local
+PORT=22
+ssh $DEVICE -p $PORT "rm -rf /var/mobile/Documents/basebin.tar"
+scp -P$PORT ../Dopamine/Dopamine/bootstrap/basebin.tar $DEVICE:/var/mobile/Documents/basebin.tar
+ssh $DEVICE -p $PORT "/var/jb/basebin/jbctl update basebin /var/mobile/Documents/basebin.tar"
