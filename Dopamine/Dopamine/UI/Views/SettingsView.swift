bd768eb0830ed6910d07adf5568c613a2a9c3ad8
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-08 22:37:15 +0800

    Update SettingsView.swift

--- Dopamine/Dopamine/UI/Views/SettingsView.swift
+++ Dopamine/Dopamine/UI/Views/SettingsView.swift
@@ -10,7 +10,7 @@ import Fugu15KernelExploit
 
 struct SettingsView: View {
     
-    @AppStorage("totalJailbreaks", store: dopamineDefaults()) var totalJailbreaks: Int = 0
+    @AppStorage("total_jailbreaks", store: dopamineDefaults()) var totalJailbreaks: Int = 0
     @AppStorage("successful_jailbreaks", store: dopamineDefaults()) var successfulJailbreaks: Int = 0
     
     @AppStorage("verboseLogsEnabled", store: dopamineDefaults()) var verboseLogs: Bool = false
