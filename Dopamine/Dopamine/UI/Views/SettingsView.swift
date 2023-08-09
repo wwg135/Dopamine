3f5e52dfec42be3f68f3f37aeddea747aa104556
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-09 20:07:39 +0800

    Update SettingsView.swift

--- Dopamine/Dopamine/UI/Views/SettingsView.swift
+++ Dopamine/Dopamine/UI/Views/SettingsView.swift
@@ -77,23 +77,25 @@ struct SettingsView: View {
                         if isBootstrapped() {
                             VStack {
                                 if isJailbroken() {
-                                    Button(action: {
-                                        UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                                        customforbidunjectAlertShown = true
-                                    }) {
-                                        HStack {
-                                            Image(systemName: "eye")
-                                            Text("Options_Custom_Forbid_Unject")
-                                                .lineLimit(1)
-                                                .minimumScaleFactor(0.5)
+                                    if forbidUnject {
+                                        Button(action: {
+                                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
+                                            customforbidunjectAlertShown = true
+                                        }) {
+                                            HStack {
+                                                Image(systemName: "eye")
+                                                Text("Options_Custom_Forbid_Unject")
+                                                    .lineLimit(1)
+                                                    .minimumScaleFactor(0.5)
+                                            }
+                                            .padding(.horizontal, 4)
+                                            .padding(8)
+                                            .frame(maxWidth: .infinity)
+                                            .overlay(
+                                                RoundedRectangle(cornerRadius: 8)
+                                                    .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
+                                            )
                                         }
-                                        .padding(.horizontal, 4)
-                                        .padding(8)
-                                        .frame(maxWidth: .infinity)
-                                        .overlay(
-                                            RoundedRectangle(cornerRadius: 8)
-                                                .stroke(Color.white.opacity(0.25), lineWidth: 0.5)
-                                        )
                                     }
                                     if enableMount {
                                         Button(action: {
