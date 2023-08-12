9cbabd80d7493c2d6b364c7be790e6e6025f834f
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-12 15:38:16 +0800

    fix Some problems (#138)

--- BaseBin/libjailbreak/src/handoff.m
+++ BaseBin/libjailbreak/src/handoff.m
@@ -9,16 +9,16 @@
 #define PPL_MAP_ADDR         0x2000000 // This is essentially guaranteed to be unused, minimum address is usually 0x100000000
 
 kern_return_t pmap_enter_options_addr(uint64_t pmap, uint64_t pa, uint64_t va) {
-    while (1) {
-        kern_return_t kr = (kern_return_t) kcall8(bootInfo_getSlidUInt64(@"pmap_enter_options_addr"), pmap, va, pa, VM_PROT_READ | VM_PROT_WRITE, 0, 0, 1, 1);
-        if (kr != KERN_RESOURCE_SHORTAGE) {
-            return kr;
+        while (1) {
+                kern_return_t kr = (kern_return_t) kcall8(bootInfo_getSlidUInt64(@"pmap_enter_options_addr"), pmap, va, pa, VM_PROT_READ | VM_PROT_WRITE, 0, 0, 1, 1);
+                if (kr != KERN_RESOURCE_SHORTAGE) {
+                        return kr;
+                }
         }
-    }
 }
 
 void pmap_remove(uint64_t pmap, uint64_t start, uint64_t end) {
-    kcall8(bootInfo_getSlidUInt64(@"pmap_remove_options"), pmap, start, end, 0x100, 0, 0, 0, 0);
+        kcall8(bootInfo_getSlidUInt64(@"pmap_remove_options"), pmap, start, end, 0x100, 0, 0, 0, 0);
 }
 
 int handoffPPLPrimitives(pid_t pid, uint64_t *magicPageOut)
