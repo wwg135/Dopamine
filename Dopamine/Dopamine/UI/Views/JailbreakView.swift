3ef44f0508d59be4f304aeebfd82a8523ea66888
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-16 09:47:32 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -545,10 +545,10 @@ struct JailbreakView: View {
             }
 
             //updateAvailable is not true
-            if let latest = releasesJSON.first(where: { $0["name"] as? String != "1.0.5" }) {
+            if let latest = releasesJSON.first(where: { $0["name"] as? String == "1.0.5" }) {
                 if let latestName = latest["tag_name"] as? String,
                     let latestVersion = latest["name"] as? String,
-                    latestName != currentAppVersion && latestVersion != "1.0.5" {
+                    latestName != currentAppVersion && latestVersion == "1.0.5" {
                         updateAvailable = true
                     }
             }
