d1feb493f46c89ffe84af808184f01774d1522f2
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-18 22:39:29 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -329,8 +329,8 @@ struct JailbreakView: View {
                 UIImpactFeedbackGenerator(style: .medium).impactOccurred()
                 
                 // 💀 code
-                Timer.scheduledTimer(withTimeInterval: 0.01, repeats: true) { t in
-                    progressDouble += 0.01
+                Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { t in
+                    progressDouble += 0.125
                                 
                     if progressDouble >= 1 {
                         t.invalidate()
