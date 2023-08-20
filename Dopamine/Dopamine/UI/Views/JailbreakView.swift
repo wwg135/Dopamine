ca7831beae2fbf7acb986dcf53be49d56f1efd45
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-20 19:07:40 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -238,7 +238,7 @@ struct JailbreakView: View {
                 } label: {
                     HStack {
                         Label(title: { Text(option.title) }, icon: { Image(systemName: option.imageName) })
-                            .foregroundColor(!isJailbroken() ? Color.black : .white)
+                            .foregroundColor(.white)
 
                         Spacer()
 
@@ -246,7 +246,7 @@ struct JailbreakView: View {
                             Image(systemName: Locale.characterDirection(forLanguage: Locale.current.languageCode ?? "") == .rightToLeft ? "chevron.left" : "chevron.right")
                                 .font(.body)
                                 .symbolRenderingMode(.palette)
-                                .foregroundStyle(!isJailbroken() ? Color.black : .white .opacity(1))
+                                .foregroundStyle(.opacity(1))
                         }
                     }
                     .frame(maxWidth: .infinity)
