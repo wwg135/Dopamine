4b960a88b2b19be3d464c46e4f83701fc5f26a6e
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-25 22:18:16 +0800

    Update UpdateDownloadingView.swift

--- Dopamine/Dopamine/UI/Views/UpdateDownloadingView.swift
+++ Dopamine/Dopamine/UI/Views/UpdateDownloadingView.swift
@@ -34,55 +34,52 @@ struct UpdateDownloadingView: View {
 
                 VStack(spacing: 16) {
                     if !showButton {
-                        VStack {
-                            VStack(spacing: 150) {
-                                VStack(spacing: 10) {
-                                    Spacer()
-                                    Text(updateState != .updating ? NSLocalizedString("Update_Status_Downloading", comment: "") : NSLocalizedString("Update_Status_Installing", comment: ""))
-                                        .font(.title2)
-                                        .multilineTextAlignment(.center)
-                                        .drawingGroup()
-                                    Text(updateState == .downloading ? NSLocalizedString("Update_Status_Subtitle_Please_Wait", comment: "") : NSLocalizedString("Update_Status_Subtitle_Restart_Soon", comment: ""))
-                                        .opacity(0.5)
-                                        .multilineTextAlignment(.center)
-                                        .padding(.bottom, 32)
-                                }
-                                .animation(.spring(), value: updateState)
+                        VStack(spacing: 150) {
+                            VStack(spacing: 10) {
+                                Spacer()
+                                Text(updateState != .updating ? NSLocalizedString("Update_Status_Downloading", comment: "") : NSLocalizedString("Update_Status_Installing", comment: ""))
+                                    .font(.title2)
+                                    .multilineTextAlignment(.center)
+                                    .drawingGroup()
+                                Text(updateState == .downloading ? NSLocalizedString("Update_Status_Subtitle_Please_Wait", comment: "") : NSLocalizedString("Update_Status_Subtitle_Restart_Soon", comment: ""))
+                                    .opacity(0.5)
+                                    .multilineTextAlignment(.center)
+                                    .padding(.bottom, 32)
                             }
-                            ZStack {
-                                ZStack {
-                                    Text("\(Int(progressDouble * 100))%")
-                                        .font(.title)
-                                        .opacity(updateState == .downloading ? 1 : 0)
-                                    if type != nil {
-                                        LoadingIndicator(animation: .circleRunner, color: .white, size: .medium, speed: .normal)
-                                            .opacity(updateState == .updating ? 1 : 0)
-                                    }
-                                }
-                                Circle()
-                                    .stroke(
-                                        Color.white.opacity(0.1),
-                                        lineWidth: updateState == .downloading ? 8 : 4
-                                    )
-                                    .animation(.spring(), value: updateState)
-                                Circle()
-                                    .trim(from: 0, to: progressDouble)
-                                    .stroke(
-                                        Color.white,
-                                        style: StrokeStyle(
-                                            lineWidth: updateState == .downloading ? 8 : 0,
-                                            lineCap: .round
-                                        )
-                                    )
-                                    .rotationEffect(.degrees(-90))
-                                    .animation(.easeOut, value: progressDouble)
-                                    .animation(.spring(), value: updateState)
-                            }
+                            .animation(.spring(), value: updateState)
                             .frame(height: 225)
-                            .padding(32)
                         }
-                        .animation(.spring(), value: updateState)
-                        .frame(maxHeight: .infinity, alignment: .center)
+                        ZStack {
+                            ZStack {
+                                Text("\(Int(progressDouble * 100))%")
+                                    .font(.title)
+                                    .opacity(updateState == .downloading ? 1 : 0)
+                                if type != nil {
+                                    LoadingIndicator(animation: .circleRunner, color: .white, size: .medium, speed: .normal)
+                                        .opacity(updateState == .updating ? 1 : 0)
+                                }
+                            }
+                            Circle()
+                                .stroke(
+                                    Color.white.opacity(0.1),
+                                    lineWidth: updateState == .downloading ? 8 : 4
+                                )
+                                .animation(.spring(), value: updateState)
+                            Circle()
+                                .trim(from: 0, to: progressDouble)
+                                .stroke(
+                                    Color.white,
+                                    style: StrokeStyle(
+                                        lineWidth: updateState == .downloading ? 8 : 0,
+                                        lineCap: .round
+                                    )
+                                )
+                                .rotationEffect(.degrees(-90))
+                                .animation(.easeOut, value: progressDouble)
+                                .animation(.spring(), value: updateState)
+                        }
+                        .frame(height: 128)
+                        .padding(32)
                     }
 
                     if showButton {
