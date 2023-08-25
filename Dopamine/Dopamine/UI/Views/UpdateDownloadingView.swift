a63daada8d1412c168020c120905891c2fc091f9
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-25 19:43:57 +0800

    Update UpdateDownloadingView.swift

--- Dopamine/Dopamine/UI/Views/UpdateDownloadingView.swift
+++ Dopamine/Dopamine/UI/Views/UpdateDownloadingView.swift
@@ -136,7 +136,7 @@ struct UpdateDownloadingView: View {
                     .frame(height: 128)
                     .padding(32)
                 }
-                .opacity(updateState != .downloading ? 1 : 0)
+                .opacity(updateState != .updating ? 1 : 0)
                 .animation(.spring(), value: updateState)
                 .frame(maxWidth: 280)
             }
