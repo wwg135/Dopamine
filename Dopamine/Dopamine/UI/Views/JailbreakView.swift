3b66a07e94ab3e75288277c70c780824dd59b8bd
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-25 16:33:36 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -506,16 +506,16 @@ struct JailbreakView: View {
             return
         }
 
-        if let latest = releasesJSON.first(where: { $0["name"] as? String != "1.0.5" }) {
-            if let latestName = latest["tag_name"] as? String,
-               let latestVersion = latest["name"] as? String {
-                   if latestName != currentAppVersion && latestVersion != "1.0.5" && checkForUpdates {
-                        updateAvailable = true
-                    }
-               }
+        let latest = releasesJSON.first(where: { $0["name"] as? String != "1.0.5" }),
+        let latestName = latest["tag_name"] as? String,
+        let latestVersion = latest["name"] as? String {
+            if latestName != currentAppVersion && latestVersion != "1.0.5" && checkForUpdates {
+                updateAvailable = true
+            }
         }
 
-        updateChangelog = createUserOrientedChangelog(deltaChangelog: getDeltaChangelog(json: releasesJSON), environmentMismatch: false)   
+        updateChangelog = createUserOrientedChangelog(deltaChangelog: getDeltaChangelog(json: releasesJSON), environmentMismatch: false)
+        
         if isInstalledEnvironmentVersionMismatching() {
             mismatchChangelog = createUserOrientedChangelog(deltaChangelog: getDeltaChangelog(json: releasesJSON), environmentMismatch: true)
         }
