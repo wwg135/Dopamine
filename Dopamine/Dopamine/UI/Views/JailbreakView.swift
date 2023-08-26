0775016474a181e29245a9ed2faccedd4824f471
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-26 21:25:14 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -635,7 +635,6 @@ struct JailbreakView_Previews: PreviewProvider {
 
 extension View {
     func placeholder<Content: View>(
-        when shouldShow: Bool,
         alignment: Alignment = .leading,
         @ViewBuilder placeholder: () -> Content) -> some View {            
             ZStack(alignment: alignment) {
