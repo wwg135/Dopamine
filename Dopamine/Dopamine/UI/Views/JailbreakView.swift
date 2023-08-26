06585ff90334c4f82a2bd728160e535c4cb8cbec
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-26 17:02:18 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -146,6 +146,8 @@ struct JailbreakView: View {
                     .frame(maxWidth: 280, maxHeight: 480)
                 }, isPresented: $isUpdatelogPresented)
                 .zIndex(2)
+
+                UpdateDownloadingView(type: $showingUpdatePopupType)
             }
             .animation(.default, value: showingUpdatePopupType == nil)
         }
