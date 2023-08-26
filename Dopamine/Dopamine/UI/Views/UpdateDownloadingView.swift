f302c679c0ddbd455a5c63b35fa4e975e8d9278f
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-26 17:00:39 +0800

    Update UpdateDownloadingView.swift

--- Dopamine/Dopamine/UI/Views/UpdateDownloadingView.swift
+++ Dopamine/Dopamine/UI/Views/UpdateDownloadingView.swift
@@ -81,106 +81,11 @@ struct UpdateDownloadingView: View {
                         .frame(height: 128)
                         .padding(32)
                     }
-
-                    if showButton {
-                        Button {
-                            UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                            showButton = false
-                            if type == .regular {
-                                updateState = .downloading
-                            
-                                // 💀 code
-                                Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { t in
-                                    progressDouble = downloadProgress.fractionCompleted
-                                
-                                    if progressDouble == 1 {
-                                        t.invalidate()
-                                    }
-                                }
-                            
-                                Task {
-                                    do {
-                                        try await downloadUpdateAndInstall()
-                                        updateState = .updating
-                                    } catch {
-                                        Logger.log("Error: \(error.localizedDescription)", type: .error)
-                                    }
-                                }
-                            } else {
-                                updateState = .updating
-                                DispatchQueue.global(qos: .userInitiated).async {
-                                    updateEnvironment()
-                                }
-                            }  
-                        } label: {
-                            Label(title: { Text("Button_Update") }, icon: { Image(systemName: "arrow.down") })
-                                .foregroundColor(.white)
-                                .padding()
-                                .frame(maxWidth: 280)
-                                .background(MaterialView(.light)
-                                    .opacity(0.5)
-                                    .cornerRadius(8)
-                                )
-                        }
-                        .fixedSize()
-                           
-                        Button {
-                            type = nil
-                        } label: {
-                            Label(title: { Text("Button_Cancel")  }, icon: { Image(systemName: "xmark") })
-                                .foregroundColor(.white)
-                                .padding()
-                                .frame(maxWidth: 280)
-                        }
-                        .fixedSize()
-                    }
                 }
             }
         }
         .foregroundColor(.white)
     }
-    
-    func downloadUpdateAndInstall() async throws {
-        let owner = "wwg135"
-        let repo = "Dopamine"
-        
-        // Get the releases
-        let releasesURL = URL(string: "https://api.github.com/repos/\(owner)/\(repo)/releases")!
-        let releasesRequest = URLRequest(url: releasesURL)
-        let (releasesData, _) = try await URLSession.shared.data(for: releasesRequest)
-        let releasesJSON = try JSONSerialization.jsonObject(with: releasesData, options: []) as! [[String: Any]]
-        
-        Logger.log(String(data: releasesData, encoding: .utf8) ?? "none")
-
-        // Find the latest release
-        guard let latestRelease = releasesJSON.first(where: { $0["name"] as? String != "1.0.5" }),
-              let assets = latestRelease["assets"] as? [[String: Any]],
-              let asset = assets.first(where: { ($0["name"] as! String).contains(".ipa") }),
-              let downloadURLString = asset["browser_download_url"] as? String,
-              let downloadURL = URL(string: downloadURLString) else {
-            throw "Could not find download URL for ipa"
-        }
-
-        // Download the asset
-        try await withThrowingTaskGroup(of: Void.self) { group in
-            downloadProgress.totalUnitCount = 1
-            group.addTask {
-                let (url, _) = try await URLSession.shared.download(from: downloadURL, progress: downloadProgress)
-                if isJailbroken() {
-                    update(tipaURL: url)
-                } else {
-                    guard let dopamineUpdateURL = URL(string: "apple-magnifier://install?url=\(url.absoluteString)") else {
-                        return
-                    }
-                    await UIApplication.shared.open(dopamineUpdateURL)
-                    exit(0)
-                    return
-                }
-            }
-            try await group.waitForAll()
-        }
-    }
-}
 
 struct UpdateDownloadingView_Previews: PreviewProvider {
     static var previews: some View {
