783f08e7db46e8619b1d7083cb7ac6a136d42539
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-27 12:39:26 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -148,7 +148,7 @@ struct JailbreakView: View {
                                 .frame(height: 128)
                                 .padding(32)
                             }
-                            .opacity(JailbreakingProgress != jailbreaking ? 1 : 0)
+                            .opacity(JailbreakingProgress != .jailbreaking ? 1 : 0)
                             .animation(.spring(), value: updateState)
                             .frame(maxWidth: 280)
                         }
