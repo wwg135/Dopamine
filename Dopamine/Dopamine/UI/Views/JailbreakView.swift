4b5e598e8db714f16d8004f3c3044733a8832c3d
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-08-27 15:30:02 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -107,58 +107,51 @@ struct JailbreakView: View {
           
                 if showDownloadPage {
                     ZStack {
-                        ZStack {
-                            VStack {
-                                Spacer()
-                                VStack {
-                                    Text(updateState != .updating ? NSLocalizedString("Update_Status_Downloading", comment: "") : NSLocalizedString("Update_Status_Installing", comment: ""))
-                                        .font(.title2)
-                                        .multilineTextAlignment(.center)
-                                        .drawingGroup()
-                                    Text(updateState == .downloading ? NSLocalizedString("Update_Status_Subtitle_Please_Wait", comment: "") : NSLocalizedString("Update_Status_Subtitle_Restart_Soon", comment: ""))
-                                        .opacity(0.5)
-                                        .multilineTextAlignment(.center)
-                                        .padding(.bottom, 32)
-                                }
-                                .animation(.spring(), value: updateState)
-                                .frame(height: 225)
-                                Spacer()
-                            }
-                            ZStack {
-                                ZStack {
-                                    Text("\(Int(progressDouble * 100))%")
-                                        .font(.title)
-                                        .opacity(updateState == .downloading ? 1 : 0) 
-                                    if updateState == .downloading || updateState == .updating {
-                                        LoadingIndicator(animation: .circleRunner, color: .white, size: .medium, speed: .normal)
-                                            .opacity(updateState == .updating ? 1 : 0)
-                                    }
-                                }
-                                Circle()
-                                    .stroke(
-                                        Color.white.opacity(0.1),
-                                        lineWidth: updateState == .downloading ? 8 : 4
-                                    )
-                                    .animation(.spring(), value: updateState)
-                                Circle()
-                                    .trim(from: 0, to: progressDouble)
-                                    .stroke(
-                                        Color.white,
-                                        style: StrokeStyle(
-                                            lineWidth: updateState == .downloading ? 8 : 0,
-                                            lineCap: .round
-                                        )
-                                    )
-                                    .rotationEffect(.degrees(-90))
-                                    .animation(.easeOut, value: progressDouble)
-                                    .animation(.spring(), value: updateState)
-                            }
-                            .frame(height: 128)
-                            .padding(32)
+                        VStack {
+                            Text(updateState != .updating ? NSLocalizedString("Update_Status_Downloading", comment: "") : NSLocalizedString("Update_Status_Installing", comment: ""))
+                                .font(.title2)
+                                .multilineTextAlignment(.center)
+                                .drawingGroup()
+                            Text(updateState == .downloading ? NSLocalizedString("Update_Status_Subtitle_Please_Wait", comment: "") : NSLocalizedString("Update_Status_Subtitle_Restart_Soon", comment: ""))
+                                .opacity(0.5)
+                                .multilineTextAlignment(.center)
+                                .padding(.bottom, 32)
                         }
-                        .opacity((updateState == .downloading || updateState == .updating) ? 1 : 0)
                         .animation(.spring(), value: updateState)
-                        .frame(maxWidth: 280)
+                        .frame(height: 225)
+                        .padding(.top, 10)
+                            
+                        ZStack {
+                            ZStack {
+                                Text("\(Int(progressDouble * 100))%")
+                                    .font(.title)
+                                    .opacity(updateState == .downloading ? 1 : 0) 
+                                if updateState == .downloading || updateState == .updating {
+                                    LoadingIndicator(animation: .circleRunner, color: .white, size: .medium, speed: .normal)
+                                        .opacity(updateState == .updating ? 1 : 0)
+                                }
+                            }
+                            Circle()
+                                .stroke(
+                                    Color.white.opacity(0.1),
+                                    lineWidth: updateState == .downloading ? 8 : 4
+                                )
+                                .animation(.spring(), value: updateState)
+                            Circle()
+                                .trim(from: 0, to: progressDouble)
+                                .stroke(
+                                    Color.white,
+                                    style: StrokeStyle(
+                                        lineWidth: updateState == .downloading ? 8 : 0,
+                                        lineCap: .round
+                                    )
+                                )
+                                .rotationEffect(.degrees(-90))
+                                .animation(.easeOut, value: progressDouble)
+                                .animation(.spring(), value: updateState)
+                        }
+                        .frame(height: 128)
+                        .padding(32)
                     }
                     .background(MaterialView(.systemUltraThinMaterialDark) .opacity(1))
                     .cornerRadius(16)
