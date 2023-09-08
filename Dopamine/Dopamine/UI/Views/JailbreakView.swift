ec28d44af4c38bd0c07c46aef59cc7824d193667
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-08 14:14:11 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -149,7 +149,7 @@ struct JailbreakView: View {
                                                     .gesture(TapGesture().onEnded {
                                                         checklog.toggle()
                                                         UIImpactFeedbackGenerator(style: .light).impactOccurred()
-                                                        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                                                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
                                                             showDownloadPage = true
                                                             updateAvailable = false
                                                             DispatchQueue.global(qos: .userInitiated).async {
