23f82ef53ffb1eaf0e124f601f170463259e6eb1
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-09 16:01:36 +0800

    Update SettingsView.swift

--- Dopamine/Dopamine/UI/Views/SettingsView.swift
+++ Dopamine/Dopamine/UI/Views/SettingsView.swift
@@ -16,7 +16,7 @@ struct SettingsView: View {
     @AppStorage("tweakInjectionEnabled", store: dopamineDefaults()) var tweakInjection: Bool = true
     @AppStorage("iDownloadEnabled", store: dopamineDefaults()) var enableiDownload: Bool = false
     @AppStorage("forbidUnject", store: dopamineDefaults()) var forbidUnject: Bool = true
-    @State var hiddenFunction = dopamineDefaults().standard.bool(forKey: "hiddenFunction") 
+    @State var hiddenFunction = UserDefaults.standard.bool(forKey: "hiddenFunction") 
     @Binding var isPresented: Bool   
     @State var mobilePasswordChangeAlertShown = false
     @State var mobilePasswordInput = "alpine"
@@ -157,7 +157,7 @@ struct SettingsView: View {
                         .opacity(1)
                         .onTapGesture(count: 1) {
                             hiddenFunction.toggle()
-                            dopamineDefaults().standard.set(hiddenFunction, forKey: "hiddenFunction")
+                            UserDefaults.standard.set(hiddenFunction, forKey: "hiddenFunction")
                             UIImpactFeedbackGenerator(style: .light).impactOccurred()
                         }
                     }
