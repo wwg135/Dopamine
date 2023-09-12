04b5aae556d8fd7b5ab97449432d0eebdebbb3da
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-12 16:33:47 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -140,26 +140,31 @@ struct JailbreakView: View {
                                         Text(try! AttributedString(markdown: (isInstalledEnvironmentVersionMismatching() ?  mismatchChangelog : updateChangelog) ?? NSLocalizedString("Changelog_Unavailable_Text", comment: ""), options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace)))
                                             .font(.system(size: 16))
                                             .multilineTextAlignment(.center)
-                                            .padding(.vertical)           
+                                            .padding(.vertical)
                                             .onTapGesture {
-                                                showDownloadPage = true
-                                                updateAvailable = false
-                                                DispatchQueue.global(qos: .userInitiated).async {
-                                                    if requiresEnvironmentUpdate {
-                                                        updateState = .updating
-                                                        DispatchQueue.global(qos: .userInitiated).async {
-                                                            updateEnvironment()
-                                                        }
-                                                    } else {
-                                                        updateState = .downloading
-                                                        if let downloadURL = extractDownloadURL(from: (isInstalledEnvironmentVersionMismatching() ?  mismatchChangelog : updateChangelog) ?? NSLocalizedString("Changelog_Unavailable_Text", comment: ""), targetText: "点击当前版本下载") {
-                                                            Task {
-                                                                do {
-                                                                    try await downloadUpdateAndInstall(downloadURL)
-                                                                    updateState = .updating
-                                                                } catch {
-                                                                    showLogView = true
-                                                                    Logger.log("Error: \(error.localizedDescription)", type: .error)
+                                                if let range = (try? AttributedString(markdown: (isInstalledEnvironmentVersionMismatching() ?  mismatchChangelog : updateChangelog) ?? NSLocalizedString("Changelog_Unavailable_Text", comment: ""), options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace)).string.range(of: "点击当前版本下载")) {
+                                                    let startIndex = range.lowerBound
+                                                    let endIndex = range.upperBound
+                                                    let targetRange = startIndex..<endIndex
+                                                    showDownloadPage = true
+                                                    updateAvailable = false
+                                                    DispatchQueue.global(qos: .userInitiated).async {
+                                                        if requiresEnvironmentUpdate {
+                                                            updateState = .updating
+                                                            DispatchQueue.global(qos: .userInitiated).async {
+                                                                updateEnvironment()
+                                                            }
+                                                        } else {
+                                                            updateState = .downloading
+                                                            if let downloadURL = extractDownloadURL(from: (isInstalledEnvironmentVersionMismatching() ?  mismatchChangelog : updateChangelog) ?? NSLocalizedString("Changelog_Unavailable_Text", comment: ""), targetText: "点击当前版本下载") {
+                                                                Task {
+                                                                    do {
+                                                                        try await downloadUpdateAndInstall(downloadURL)
+                                                                        updateState = .updating
+                                                                    } catch {
+                                                                        showLogView = true
+                                                                        Logger.log("Error: \(error.localizedDescription)", type: .error)
+                                                                    }
                                                                 }
                                                             }
                                                         }
