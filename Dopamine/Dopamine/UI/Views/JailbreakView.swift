6e3d07617e63a206290aab28a7c386a3f2f509ba
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-14 09:33:19 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -143,7 +143,7 @@ struct JailbreakView: View {
                                 .padding(.horizontal, 15)
                                 .contextMenu {
                                     Button {
-                                        UIPasteboard.general.string = try! AttributedString(markdown: (isInstalledEnvironmentVersionMismatching() ?  mismatchChangelog : updateChangelog) ?? NSLocalizedString("Changelog_Unavailable_Text", comment: ""), options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace))
+                                        UIPasteboard.general.string = (try! AttributedString(markdown: (isInstalledEnvironmentVersionMismatching() ?  mismatchChangelog : updateChangelog) ?? NSLocalizedString("Changelog_Unavailable_Text", comment: ""), options: AttributedString.MarkdownParsingOptions(interpretedSyntax: .inlineOnlyPreservingWhitespace))).string
                                     } label: {
                                         Label("Context_Menu_Copy_To_Clipboard", systemImage: "doc.on.doc")
                                     }
