9bfd9ea067c484f16574008051f1119f45c5f006
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-15 09:28:57 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -390,15 +390,6 @@ struct JailbreakView: View {
                                                         isSelected ? Image(systemName: "checkmark.circle.fill") : Image(systemName: "xmark.circle.fill")
                                                     }
                                                     .foregroundColor(isSelected ? .green : .red)
-                                                    .onAppear {
-                                                        if let savedState = UserDefaults.standard.object(forKey: name) as? Bool {
-                                                            selectedNames.append(name)
-                                                            ForbidApp(name)
-                                                        } else {
-                                                            deletedNames.append(name)
-                                                            removeApp(name)
-                                                        }
-                                                    }
                                                 }
                                             }
                                         }
@@ -406,6 +397,17 @@ struct JailbreakView: View {
                                 }
                                 .opacity(1)
                                 .frame(maxWidth: 250, maxHeight: 300)
+                                .onAppear {
+                                    for name in appNames {
+                                        if UserDefaults.standard.bool(forKey: name) {
+                                            selectedNames.append(name)
+                                            ForbidApp(name)
+                                        } else {
+                                            deletedNames.append(name)
+                                            removeApp(name)
+                                        }
+                                    }
+                                }
                             }
                         }
                         .padding(.vertical)
