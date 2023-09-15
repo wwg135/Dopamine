ab6b49b52d281e61ddc449f7ed05cbe4970e2804
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-15 10:17:49 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -341,11 +341,11 @@ struct JailbreakView: View {
                                             .padding(.vertical, 5)
                                             .foregroundColor(colorScheme == .dark ? .white : .black)
                                         ForEach(appNames.sorted { (app1, app2) in
-                                            let isName1Forbidden = isAppForbidden(app1.1)
-                                            let isName2Forbidden = isAppForbidden(app2.1)  
-                                            if isName1Forbidden && !isName2Forbidden {
+                                            let isSelected1 = selectedNames.contains(app1.1)
+                                            let isSelected2 = selectedNames.contains(app2.1)
+                                            if isSelected1 && !isSelected2 {
                                                 return true
-                                            } else if !isName1Forbidden && isName2Forbidden {
+                                            } else if !isSelected1 && isSelected2 {
                                                 return false
                                             } else {
                                                 let localizedNameComparison = app1.0.localizedCompare(app2.0)
@@ -359,7 +359,7 @@ struct JailbreakView: View {
                                             if searchText.isEmpty || localizedAppName.localizedCaseInsensitiveContains(searchText) {
                                                 HStack {
                                                     let isForbidden = isAppForbidden(name)
-                                                    Text("\(localizedAppName) - \(name)\(isForbidden ? Text("   ✓").foregroundColor(.green) : Text(""))")
+                                                    Text("\(localizedAppName) - \(name)")
                                                         .font(.system(size: 16))
                                                         .padding(.vertical, 5)
                                                     Spacer()
@@ -908,17 +908,6 @@ struct JailbreakView: View {
             dict[name] = true
             dict.write(toFile: filePath, atomically: true)
         }
-    }
-
-    func isAppForbidden(_ name: String) -> Bool {
-        let fileManager = FileManager.default
-        let filePath = "/var/mobile/zp.unject.plist"
-        if fileManager.fileExists(atPath: filePath),
-        let dict = NSMutableDictionary(contentsOfFile: filePath),
-        dict[name] != nil {
-            return true
-        }
-        return false
     }
 
     func removeApp(_ name: String) {
