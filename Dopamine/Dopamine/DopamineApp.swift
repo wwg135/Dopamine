d367a3005f7b0a30bfb43c56cc8d19616e172d23
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-20 17:09:10 +0800

    Update DopamineApp.swift

--- Dopamine/Dopamine/DopamineApp.swift
+++ Dopamine/Dopamine/DopamineApp.swift
@@ -7,12 +7,20 @@
 
 import SwiftUI
 
-var whatCouldThisVariablePossiblyEvenMean = {
+var Mid-Autumn Festival = {
     let date = Date()
     let calendar = Calendar.current
     let components = calendar.dateComponents([.day, .month], from: date)
 
-    return components.day == 20 && components.month == 9
+    return components.day == 29 && components.month == 9
+}()
+
+var National Day = {
+    let date = Date()
+    let calendar = Calendar.current
+    let components = calendar.dateComponents([.day, .month], from: date)
+
+    return components.day == 1 && components.month == 10
 }()
 
 struct Fugu15App: App {
