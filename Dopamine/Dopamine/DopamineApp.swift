f6e0c3c990299c31d144ad5d76006eb0cd5497c5
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-09-20 16:59:52 +0800

    Update DopamineApp.swift

--- Dopamine/Dopamine/DopamineApp.swift
+++ Dopamine/Dopamine/DopamineApp.swift
@@ -6,42 +6,13 @@
 //
 
 import SwiftUI
-import LunarCore
 
-var isFestivalDay: Bool = {
+var whatCouldThisVariablePossiblyEvenMean = {
     let date = Date()
     let calendar = Calendar.current
-    let lunar = Lunar(date: date)
-    let day = calendar.component(.day, from: date)
-    let month = calendar.component(.month, from: date)
-    let lunarMonth = lunar.lunarMonth
-    let lunarDay = lunar.lunarDay
+    let components = calendar.dateComponents([.day, .month], from: date)
 
-    let festivalDays: [(Int, Int)] = [
-        (1, 1),   // 元旦
-        (12, 31), // 除夕
-        (1, 1),   // 春节
-        (1, 15),  // 元宵节
-        (3, 8),   // 妇女节
-        (6, 1),   // 儿童节
-        (7, 1),   // 建党节
-        (4, 4),   // 清明节
-        (5, 1),   // 劳动节
-        (5, 5),   // 端午节
-        (9, 10),  // 教师节
-        (8, 15),  // 中秋节
-        (10, 1),  // 国庆节
-        (11, 11),  // 光棍节
-        (12, 25)  // 圣诞节
-    ]
-
-    for festival in festivalDays {
-        if (festival.0 == month && festival.1 == day) || (festival.0 == lunarMonth && festival.1 == lunarDay) {
-            return true
-        }
-    }
-
-    return false
+    return components.day == 20 && components.month == 9
 }()
 
 struct Fugu15App: App {
