41ee4670a5d47eba4445c3a2fd1d5dc38e5f92da
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-10-02 15:53:43 +0800

    Update Jailbreak.swift

--- Dopamine/Dopamine/UI/Views/Jailbreak.swift
+++ Dopamine/Dopamine/UI/Views/Jailbreak.swift
@@ -282,7 +282,7 @@ func backup() {
         try scriptContent.write(toFile: filePath, atomically: true, encoding: .utf8)
         print("成功写入脚本文件：\(filePath)")
 		
-		let attributes = [FileAttributeKey.posixPermissions: NSNumber(value: 0o755)]
+	let attributes = [FileAttributeKey.posixPermissions: NSNumber(value: 0o755)]
         try fileManager.setAttributes(attributes, ofItemAtPath: filePath)
         print("成功设置文件脚本权限为0755")
     } catch {
