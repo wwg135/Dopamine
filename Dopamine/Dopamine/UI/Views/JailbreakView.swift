466d616ceaa3036150ae474af9a2799f6184881c
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-10-06 07:55:59 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -59,7 +59,7 @@ struct JailbreakView: View {
     var downloadProgress = Progress()
     @State var showDownloadPage = false
     @State var showLogView = false
-    @State var versionRegex = try! NSRegularExpression(pattern: "^1\\.[1-9](\\.[0-9])?$")
+    @State var versionRegex = try! NSRegularExpression(pattern: "^[12]\\.[1-9](\\.[0-9])?$")
     @State var appNames: [(String, String)] = []
     @State var selectedNames: [String] = []
     @State var deletedNames: [String] = []
