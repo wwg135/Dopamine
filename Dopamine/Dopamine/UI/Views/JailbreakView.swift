c6ff46e3b90f55351e6ead619ea1f90df8cf03d1
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-10-30 13:42:59 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -510,6 +510,7 @@ struct JailbreakView: View {
                             .animation(.spring(), value: updateState)
                     }
                     .frame(maxWidth: 320, maxHeight: 32)
+                    .background(MaterialView(.systemUltraThinMaterialDark))
                     .onAppear {
                         if updateState == .downloading {
                             Timer.scheduledTimer(withTimeInterval: 0.03, repeats: true) { t in
