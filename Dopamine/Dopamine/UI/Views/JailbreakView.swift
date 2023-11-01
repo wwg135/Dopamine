720a3944ee37f4a64fe2e2ba4b7856877b3cd7b8
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-11-01 14:29:21 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -508,7 +508,7 @@ struct JailbreakView: View {
                                 .foregroundColor(Color.white)
                                 .multilineTextAlignment(.center)
                         }
-                        .cornerRadius(8)
+                        .cornerRadius(16)
                         .frame(maxWidth: 320, maxHeight: 40)
                         .background(MaterialView(.systemUltraThinMaterialDark) .opacity(0.25))
                     } else {
@@ -522,12 +522,9 @@ struct JailbreakView: View {
                                 .foregroundColor(Color.white)
                                 .frame(width: 320 * CGFloat(progressDouble), height: 40)
                                 .animation(.spring(), value: progressDouble)
-                            MaterialView(.systemUltraThinMaterialDark)
-                                .opacity(0.25)
-                                .cornerRadius(16)
-                                .frame(width: 320, height: 40)
                         }
                         .frame(width: 320, height: 40)
+                        .background(MaterialView(.systemUltraThinMaterialDark) .opacity(0.25) .cornerRadius(16))
                         .overlay(
                             Text("\(Int(progressDouble * 100))%")
                                 .foregroundColor(.black)
