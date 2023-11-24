bada867a1ad397495cb3c4d305d2c9259af50ca8
wwg135 <105048516+wwg135@users.noreply.github.com>
2023-11-24 18:20:01 +0800

    Update JailbreakView.swift

--- Dopamine/Dopamine/UI/Views/JailbreakView.swift
+++ Dopamine/Dopamine/UI/Views/JailbreakView.swift
@@ -873,11 +873,11 @@ struct JailbreakView: View {
     }
     
     func extractDownloadURL(from text: String, targetText: String) -> URL? {
-        let pattern = "\\[.*?\\]\\((.*?)\\)"
+        let pattern = "\\(\\[(.*?)\\]\\((.*?)\\)\\)"
         let regex = try! NSRegularExpression(pattern: pattern, options: [])
         let range = NSRange(location: 0, length: text.utf16.count)
         if let match = regex.firstMatch(in: text, options: [], range: range) {
-            let urlRange = match.range(at: 1)
+            let urlRange = match.range(at: 2)
             if let url = URL(string: (text as NSString).substring(with: urlRange)) {
                 return url
             }
