de46390a7e74aaa7034007fec0fbd88447b9035c
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-02-21 18:04:26 +0800

    Update DOActionMenuView.h

--- Application/Dopamine/UI/ActionMenu/DOActionMenuView.h
+++ Application/Dopamine/UI/ActionMenu/DOActionMenuView.h
@@ -18,7 +18,6 @@ NS_ASSUME_NONNULL_BEGIN
 
 - (instancetype)initWithActions:(NSArray<UIAction*> *)actions delegate:(id<DOActionMenuDelegate>)delegate;
 - (void)hide;
-- (void)setContextMenu:(UIMenu *)menu forIdentifier:(NSString *)identifier;
 
 @end
 
