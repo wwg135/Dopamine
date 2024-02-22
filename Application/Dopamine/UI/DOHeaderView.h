88ca937b6840ccd7624a6311fe26d0d2cfb7f8bf
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-02-22 15:38:20 +0800

    Update DOHeaderView.h

--- Application/Dopamine/UI/DOHeaderView.h
+++ Application/Dopamine/UI/DOHeaderView.h
@@ -11,7 +11,9 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface DOHeaderView : UIView
 
--(id)initWithImage:(UIImage *)image subtitles:(NSArray<NSAttributedString *> *)subtitles;
+@property (nonatomic, strong) NSMutableArray<NSAttributedString *> *subtitles;
+
+-(instancetype)initWithImage:(UIImage *)image subtitles:(NSArray<NSAttributedString *> *)subtitles;
 
 @end
 
