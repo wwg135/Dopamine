63219c20d17829851fd13d498a64beff648b0be2
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-02-23 17:47:10 +0800

    Update DOUIManager.m

--- Application/Dopamine/UI/DOUIManager.m
+++ Application/Dopamine/UI/DOUIManager.m
@@ -33,6 +33,10 @@
 
 - (BOOL)isUpdateAvailable
 {
+    NSNumber *checkforUpdate = [_preferenceManager preferenceValueForKey:@"checkforUpdateEnabled"];
+    if ([checkforUpdate boolValue] == NO)
+        return NO;
+        
     NSArray *releases = [self getLatestReleases];
     if (releases.count == 0)
         return NO;
