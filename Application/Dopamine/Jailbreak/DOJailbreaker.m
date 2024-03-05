f760b5c2d73346afa9be03818fcf107a01658fa4
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-03-05 08:02:38 +0800

    Update DOJailbreaker.m

--- Application/Dopamine/Jailbreak/DOJailbreaker.m
+++ Application/Dopamine/Jailbreak/DOJailbreaker.m
@@ -536,7 +536,7 @@ typedef NS_ENUM(NSInteger, JBErrorCode) {
     // Note: This causes the app to freeze in some instances due to launchd only having physrw_pte, we might want to only do it when neccessary
     // It's only neccessary when we don't immediately userspace reboot
     
-    printf("越狱成功!\n");
+    printf("Done!\n");
 }
 
 - (void)finalize
