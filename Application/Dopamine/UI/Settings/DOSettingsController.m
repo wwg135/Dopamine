1311561af3cf9f447fe61eee7ed79d7aa49cca62
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-03-20 22:12:19 +0800

    Update DOSettingsController.m

--- Application/Dopamine/UI/Settings/DOSettingsController.m
+++ Application/Dopamine/UI/Settings/DOSettingsController.m
@@ -132,7 +132,7 @@
         
         PSSpecifier *headerSpecifier = [PSSpecifier emptyGroupSpecifier];
         [headerSpecifier setProperty:@"DOHeaderCell" forKey:@"headerCellClass"];
-        [headerSpecifier setProperty:[NSString stringWithFormat:@"Settings"] forKey:@"title"];
+        [headerSpecifier setProperty:[NSString stringWithFormat:DOLocalizedString(@"Settings")] forKey:@"title"];
         [specifiers addObject:headerSpecifier];
         
         if (!envManager.isJailbroken) {
@@ -185,6 +185,12 @@
         [tweakInjectionSpecifier setProperty:@"tweakInjectionEnabled" forKey:@"key"];
         [tweakInjectionSpecifier setProperty:@YES forKey:@"default"];
         [specifiers addObject:tweakInjectionSpecifier];
+
+        PSSpecifier *checkForUpdateSpecifier = [PSSpecifier preferenceSpecifierNamed:DOLocalizedString(@"Settings_Check_For_Update") target:self set:defSetter get:defGetter detail:nil cell:PSSwitchCell edit:nil];
+        [checkForUpdateSpecifier setProperty:@YES forKey:@"enabled"];
+        [checkForUpdateSpecifier setProperty:@"checkForUpdateEnabled" forKey:@"key"];
+        [checkForUpdateSpecifier setProperty:@NO forKey:@"default"];
+        [specifiers addObject:checkForUpdateSpecifier];
         
         if (!envManager.isJailbroken) {
             PSSpecifier *verboseLogSpecifier = [PSSpecifier preferenceSpecifierNamed:DOLocalizedString(@"Settings_Verbose_Logs") target:self set:defSetter get:defGetter detail:nil cell:PSSwitchCell edit:nil];
