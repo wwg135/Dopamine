d0c0c545cad48058bc62fb85737126fda5a51a22
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-03-27 21:52:51 +0800

    Update DONavigationController.m

--- Application/Dopamine/UI/DONavigationController.m
+++ Application/Dopamine/UI/DONavigationController.m
@@ -28,15 +28,6 @@
 
 @implementation DONavigationController
 
-+ (instancetype)sharedInstance {
-    static DONavigationController *sharedInstance = nil;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        sharedInstance = [[self alloc] init];
-    });
-    return sharedInstance;
-}
-
 - (void)viewDidLoad
 {
     [self setupBackground];
