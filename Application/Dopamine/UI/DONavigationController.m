3a8121824d3cb8008fc9659448aa2efbb1ede97d
wwg135 <105048516+wwg135@users.noreply.github.com>
2024-03-27 19:46:10 +0800

    Update DONavigationController.m

--- Application/Dopamine/UI/DONavigationController.m
+++ Application/Dopamine/UI/DONavigationController.m
@@ -141,7 +141,7 @@
       return [[[UIImage imageNamed:image] imageWithBlur:18.0] imageWithHue: M_PI * 2];  
 }
 
-- (void)handleSingleTap:(UITapGestureRecognizer *)tap {
+- (void)handleSingleTap:(UITapGestureRecognizer *)gestureRecognizer {
     if (gestureRecognizer.state == UIGestureRecognizerStateBegan) {
         UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet]; 
         DOEnvironmentManager *envManager = [DOEnvironmentManager sharedManager];
