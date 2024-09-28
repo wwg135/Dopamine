983b6f411e17f67f07033ac4d6f325c72a275502
cuucondiep <59134133+cuucondiep@users.noreply.github.com>
2024-09-28 15:56:20 +0700

    Update DONavigationController.m

--- Application/Dopamine/UI/DONavigationController.m
+++ Application/Dopamine/UI/DONavigationController.m
@@ -140,12 +140,12 @@
         UIAlertController *alertController = [UIAlertController alertControllerWithTitle:nil message:nil preferredStyle:UIAlertControllerStyleActionSheet]; 
         DOEnvironmentManager *envManager = [DOEnvironmentManager sharedManager];
         if (envManager.isJailbroken) {
-            [alertController addAction:[UIAlertAction actionWithTitle:@"重启设备" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            [alertController addAction:[UIAlertAction actionWithTitle:@"Reboot" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
                 [[DOEnvironmentManager sharedManager] reboot];
             }]];
         }
 
-        [alertController addAction:[UIAlertAction actionWithTitle:@"自定义主题背景" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Chọn ảnh nền" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             UIImagePickerController *imagePicker = [[UIImagePickerController alloc] init];
             imagePicker.delegate = self;
             imagePicker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
@@ -154,7 +154,7 @@
             [viewController presentViewController:imagePicker animated:YES completion:nil];
         }]];
 
-        [alertController addAction:[UIAlertAction actionWithTitle:@"恢复默认主题背景" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Khôi phục chủ đề mặc định" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
             DOTheme *theme = [[DOThemeManager sharedInstance] enabledTheme];
             self.selectedBackgroundImage = nil;
             [self.backgroundImageView setImage:theme.image];
@@ -164,7 +164,7 @@
             [defaults synchronize];
         }]];
 
-        [alertController addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil]];       
+        [alertController addAction:[UIAlertAction actionWithTitle:@"Xoá" style:UIAlertActionStyleCancel handler:nil]];       
         [self presentViewController:alertController animated:YES completion:nil];
     }
 }
