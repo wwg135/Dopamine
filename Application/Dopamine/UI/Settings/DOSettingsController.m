0bced82cc09ceb9573b051d73aad4028edf01603
w2599 <114452622+w2599@users.noreply.github.com>
2024-10-25 19:59:22 +0800

    jetsam 1.25x  1.75x

--- Application/Dopamine/UI/Settings/DOSettingsController.m
+++ Application/Dopamine/UI/Settings/DOSettingsController.m
@@ -120,7 +120,9 @@
 {
     return @[
     @2,
+    @2.5,
     @3,
+    @3.5,
     @4,
     @5,
     @6,
@@ -132,13 +134,15 @@
 - (NSArray *)jetsamOptionTitles
 {
     return @[
-        @"1x",
-        @"1.5x",
-        @"2x",
-        @"2.5x",
-        [NSString stringWithFormat:@"3x (%@)", DOLocalizedString(@"Recommended")],
-        @"3.5x",
-        @"4x",
+        @"1.00x",
+        @"1.25x",
+        @"1.50x",
+        @"1.75x",
+        @"2.00x",
+        @"2.50x",
+        [NSString stringWithFormat:@"3.00x (%@)", DOLocalizedString(@"Recommended")],
+        @"3.50x",
+        @"4.00x",
     ];
 }
 
@@ -476,7 +480,7 @@
     DOEnvironmentManager *envManager = [DOEnvironmentManager sharedManager];
     if (envManager.isJailbroken) {
         double v = jbclient_jbsettings_get_double("jetsamMultiplier");
-        return @((v < 1 || isnan(v)) ? 6 : ceil(v * 2));
+        return @((v < 1 || isnan(v)) ? 6 : (v * 2));
     }
     return [self readPreferenceValue:specifier];
 }
