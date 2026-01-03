wwg135/Dopamine

commit 3fc45039b98ab8550a5e06fd286678aa855a7a20

pmap_map_in: Fix bug when PA and UA have different alignments, optimize better

BaseBin/libjailbreak/src/util.c
@@ -370,22 +370,26 @@ int pmap_map_in(uint64_t pmap, uint64_t uaStart, uint64_t paStart, uint64_t size
 
 	// Allocate all page tables that need to be allocated
 	if (pmap_expand_range(pmap, uaStart, size) != 0) return -1;
-	
+
 	// Insert entries into L3 pages
+	uint64_t curPA = paStart;
 	for (uint64_t i = 0; i < l2Count; i++) {
 		uint64_t uaL2Cur = uaL2Start + (i * L2_BLOCK_SIZE);
-		uint64_t paL2Cur = paL2Start + (i * L2_BLOCK_SIZE);
+
+		// Current L2 range
+		uint64_t uaL2CurStart = uaL2Cur;
+		uint64_t uaL2CurEnd = uaL2Cur + L2_BLOCK_SIZE;
+
+		// Round to passed boundary if neccessary
+		if (uaStart > uaL2CurStart) uaL2CurStart = uaStart;
+		if (uaEnd < uaL2CurEnd) uaL2CurEnd = uaEnd;
 
 		// Create full table for this mapping
 		uint64_t tableToWrite[L2_BLOCK_COUNT];
-		for (int k = 0; k < L2_BLOCK_COUNT; k++) {
-			uint64_t curMappingPage = paL2Cur + (k * vm_real_kernel_page_size);
-			if (curMappingPage >= paStart && curMappingPage < paEnd) {
-				tableToWrite[k] = curMappingPage | PERM_TO_PTE(PERM_KRW_URW) | PTE_NON_GLOBAL | PTE_OUTER_SHAREABLE | PTE_LEVEL3_ENTRY;
-			}
-			else {
-				tableToWrite[k] = 0;
-			}
+		memset(tableToWrite, 0, sizeof(tableToWrite));
+		for (uint64_t curUA = uaL2CurStart; curUA < uaL2CurEnd; curUA += 0x4000, curPA += 0x4000) {
+			int idx = (curUA - uaL2Cur) / 0x4000;
+			tableToWrite[idx] = curPA | PERM_TO_PTE(PERM_KRW_URW) | PTE_NON_GLOBAL | PTE_OUTER_SHAREABLE | PTE_LEVEL3_ENTRY;
 		}
 
 		// Replace table with the entries we generated
@@ -398,8 +402,8 @@ int pmap_map_in(uint64_t pmap, uint64_t uaStart, uint64_t paStart, uint64_t size
 	return 0;
 }
 
-
 #ifdef __arm64e__
+
 uint64_t pmap_find_main_binary_code_dir(uint64_t pmap)
 {
 	uint64_t mainCodeDir = 0;

commit 59ada54c1b476d120b99383ce01938a31c8d929c

Fix A8 support (regression in commit 3fc4503)

BaseBin/libjailbreak/src/util.c
@@ -389,8 +389,8 @@ int pmap_map_in(uint64_t pmap, uint64_t uaStart, uint64_t paStart, uint64_t size
 		// Create full table for this mapping
 		uint64_t tableToWrite[L2_BLOCK_COUNT];
 		memset(tableToWrite, 0, sizeof(tableToWrite));
-		for (uint64_t curUA = uaL2CurStart; curUA < uaL2CurEnd; curUA += 0x4000, curPA += 0x4000) {
-			int idx = (curUA - uaL2Cur) / 0x4000;
+		for (uint64_t curUA = uaL2CurStart; curUA < uaL2CurEnd; curUA += vm_real_kernel_page_size, curPA += vm_real_kernel_page_size) {
+			int idx = (curUA - uaL2Cur) / vm_real_kernel_page_size;
 			tableToWrite[idx] = curPA | PERM_TO_PTE(PERM_KRW_URW) | PTE_NON_GLOBAL | PTE_OUTER_SHAREABLE | PTE_LEVEL3_ENTRY;
 		}
 