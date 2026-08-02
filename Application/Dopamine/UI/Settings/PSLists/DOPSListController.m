40987e4a1b3fec0c1215d12933a44f9fd0b5f2ae
opa334 <opa334@users.noreply.github.com>
2026-08-02 21:25:36 +0200

    Disable custom switch color on iOS 26.0+ because it doesn't persist through cells being reused

--- Application/Dopamine/UI/Settings/PSLists/DOPSListController.m
+++ Application/Dopamine/UI/Settings/PSLists/DOPSListController.m
@@ -31,7 +31,15 @@
     vc.view.layer.cornerRadius = 16;
     vc.view.layer.masksToBounds = YES;
     vc.view.layer.cornerCurve = kCACornerCurveContinuous;
-    [UISwitch appearanceWhenContainedInInstancesOfClasses:@[[vc class]]].onTintColor = [UIColor colorWithRed: 71.0/255.0 green: 169.0/255.0 blue: 135.0/255.0 alpha: 1.0];
+    
+    if (@available(iOS 19.0, *)) {
+        // Apple broke the method below by reimplementing some Preferences.framework classes in SwiftUI 🤮
+        // Now onTintColor is only implemented when the cell is initialized but gets reset to the stock color when the cell is reused
+        // I tried hard to fix it but failed in the end, so we will need to just use the stock tint color on iOS 26.0+
+    }
+    else {
+        [UISwitch appearanceWhenContainedInInstancesOfClasses:@[[vc class]]].onTintColor = [UIColor colorWithRed: 71.0/255.0 green: 169.0/255.0 blue: 135.0/255.0 alpha: 1.0];
+    }
 }
 
 - (void)tableView:(UITableView *)tableView willDisplayCell:(UITableViewCell *)cell forRowAtIndexPath:(NSIndexPath *)indexPath {
