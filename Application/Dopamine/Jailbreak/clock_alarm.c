aeaffc3d0be714971375a6e31619da88a689788e
opa334 <opa334@users.noreply.github.com>
2026-08-05 14:54:33 +0200

    clock_alarm: Clean up, fix iOS 15 support

--- Application/Dopamine/Jailbreak/clock_alarm.c
+++ Application/Dopamine/Jailbreak/clock_alarm.c
@@ -12,20 +12,10 @@
 #include <mach/mig.h>
 #include <mach/ndr.h>
 #include <mach-o/dyld.h>
-#include <fcntl.h>
-#include <sys/utsname.h>
-#include <sys/fileport.h>
-#include <sys/socket.h>
-#include <signal.h>
-#include <errno.h>
-#define IPPROTO_ICMPV6          58
-#define ICMP6_FILTER            18
-#include <pthread.h>
-#include <sys/uio.h>
-#include <sys/types.h>
-#include <sys/sysctl.h>
 #include <time.h>
-#include <sched.h>
+#include <dlfcn.h>
+
+#define MACH64_SEND_KOBJECT_CALL 0x0000000200000000ull
 
 typedef struct {
     mach_msg_header_t head;
@@ -42,29 +32,7 @@ typedef struct {
     kern_return_t result;
 } clock_alarm_reply_t;
 
-extern mach_msg_return_t mach_msg2_internal(
-    void *data, uint64_t option64, uint64_t msghBitsAndSendSize,
-    uint64_t msghRemoteAndLocalPort, uint64_t msghVoucherAndID,
-    uint64_t descriptorCountAndReceiveName,
-    uint64_t receiveSizeAndPriority, uint64_t timeout);
-
-#define DARKSWORD_MACH64_SEND_KOBJECT_CALL 0x0000000200000000ULL
-
-static uint64_t purple_reclaim_monotonic_nanoseconds(void)
-{
-    struct timespec value = {0};
-    if (clock_gettime(CLOCK_MONOTONIC_RAW, &value) != 0) {
-        return 0;
-    }
-    return (uint64_t)value.tv_sec * 1000000000ULL +
-        (uint64_t)value.tv_nsec;
-}
-
-
-static uint64_t purple_reclaim_pack_mach_msg2(uint32_t low, uint32_t high)
-{
-    return ((uint64_t)high << 32) | low;
-}
+typedef uint64_t mach_msg_option64_t;
 
 kern_return_t clock_alarm_preserve_port(mach_port_t surfacePort, uint32_t delaySeconds)
 {
@@ -102,20 +70,35 @@ kern_return_t clock_alarm_preserve_port(mach_port_t surfacePort, uint32_t delayS
     message.request.alarmTime.tv_sec = delaySeconds;
     message.request.alarmTime.tv_nsec = 0;
 
-    uint64_t options = MACH_SEND_MSG | MACH_RCV_MSG |
-        DARKSWORD_MACH64_SEND_KOBJECT_CALL;
-    mach_msg_return_t messageResult = mach_msg2_internal(
-        &message, options,
-        purple_reclaim_pack_mach_msg2(message.request.head.msgh_bits,
-                                      message.request.head.msgh_size),
-        purple_reclaim_pack_mach_msg2(message.request.head.msgh_remote_port,
-                                      message.request.head.msgh_local_port),
-        purple_reclaim_pack_mach_msg2(message.request.head.msgh_voucher_port,
-                                      (uint32_t)message.request.head.msgh_id),
-        purple_reclaim_pack_mach_msg2(
-            message.request.body.msgh_descriptor_count, rpcReply),
-        purple_reclaim_pack_mach_msg2(sizeof(message), 0),
-        MACH_MSG_TIMEOUT_NONE);
+    mach_msg_return_t (*mach_msg2_internal)(
+        void *data, uint64_t option64, uint64_t msghBitsAndSendSize,
+        uint64_t msghRemoteAndLocalPort, uint64_t msghVoucherAndID,
+        uint64_t descriptorCountAndReceiveName,
+        uint64_t receiveSizeAndPriority, uint64_t timeout) = dlsym(RTLD_DEFAULT, "mach_msg2_internal");
+
+    mach_msg_return_t messageResult = 0;
+    if (mach_msg2_internal) {
+#define MACH_MSG2_SHIFT_ARGS(lo, hi) ((uint64_t)hi << 32 | (uint32_t)lo)
+        messageResult = mach_msg2_internal(
+            &message,
+            MACH_SEND_MSG | MACH_RCV_MSG | MACH64_SEND_KOBJECT_CALL,
+            MACH_MSG2_SHIFT_ARGS(message.request.head.msgh_bits, message.request.head.msgh_size),
+            MACH_MSG2_SHIFT_ARGS(message.request.head.msgh_remote_port, message.request.head.msgh_local_port),
+            MACH_MSG2_SHIFT_ARGS(message.request.head.msgh_voucher_port, (uint32_t)message.request.head.msgh_id),
+            MACH_MSG2_SHIFT_ARGS(message.request.body.msgh_descriptor_count, rpcReply),
+            MACH_MSG2_SHIFT_ARGS(sizeof(message), 0),
+            MACH_MSG_TIMEOUT_NONE);
+    }
+    else {
+        messageResult = mach_msg(&message.request.head,
+            MACH_SEND_MSG | MACH_RCV_MSG,
+            message.request.head.msgh_size,
+            sizeof(message),
+            rpcReply,
+            MACH_MSG_TIMEOUT_NONE,
+            0);
+    }
+
     mach_port_deallocate(mach_task_self(), clockPort);
     if (messageResult != MACH_MSG_SUCCESS) {
         mig_dealloc_reply_port(rpcReply);
